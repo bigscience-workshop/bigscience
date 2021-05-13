9dbf5b410a2961ba4a803763fca353dc795921b0
Stas Bekman <stas@stason.org>
2021-05-13 10:30:25 -0700

    fix

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # bigscience
 
+Research workshop on large language models - The Summer of Language Models 21
+
 WIP
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     },
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='bigscience',
     name='bigscience',
