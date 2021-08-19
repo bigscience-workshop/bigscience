380bbe727187ad27f25ae85578a8e200d4513b23
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-08-19 23:20:47 +0200

    Fix dataset_probabilities generation

--- train/tr5-1B3-multilingual/generate_dataset_probabilities.py
+++ train/tr5-1B3-multilingual/generate_dataset_probabilities.py
@@ -2,6 +2,22 @@ import json
 import os
 from pathlib import Path
 
+def removeprefix(string, prefix):
+    if string.startswith(prefix):
+        string = string[len(prefix):]
+    return string
+
+def removesuffix(string, suffix):
+    if string.endswith(suffix):
+        string = string[:-len(suffix)]
+    return string
+
+def convert_path(original_path, data_path):
+    prefix_to_remove = "dumped/mc4_processed_data/"
+    suffix_to_remove = ".bin"
+
+    return data_path / removesuffix(removeprefix(original_path, prefix_to_remove), suffix_to_remove)
+
 def main():
     """Write to """
     data_path = Path(os.environ["six_ALL_CCFRSCRATCH"]) / "datasets-custom" / "mc4" / "mc4_preprocessing"
@@ -14,7 +30,7 @@ def main():
 
     # Format probabilities dictionary to store path in key and probability as value
     probabilities = {
-        data_path / key.replace("dumped/mc4_processed_data/", ""): value[0] for key, value in probabilities.items()
+        convert_path(key, data_path): value[0] for key, value in probabilities.items()
     }
 
     with open(output_path, "w") as fout:
