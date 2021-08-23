fe3d797f4bb16e986573ac979c3c69e92930d9e8
Stas Bekman <stas@stason.org>
2021-08-23 11:31:33 -0700

    add email alert feature

--- tools/slurm-status.py
+++ tools/slurm-status.py
@@ -14,12 +14,58 @@ import io
 import json
 import os
 import re
+import shlex
+import smtplib
+import socket
 import subprocess
 import sys
-import shlex
 from datetime import datetime, timedelta
 
 SLURM_GROUP_NAME = "six"
+
+# this needs to be an actual email subscribed to bigscience-jean-zay@groups.google.com
+FROM_ADDR = "bigscience-bot@huggingface.co"
+TO_ADDRS = ["stas@stason.org"] # wants a list
+# when ready switch to target ["bigscience-jean-zay@groups.google.com"]
+
+def send_email(subject, body):
+    message = f"""\
+From: {FROM_ADDR}
+To: {", ".join(TO_ADDRS)}
+Subject: {subject}
+
+{body}
+"""
+
+    server = smtplib.SMTP("localhost")
+    #server.set_debuglevel(3)  # uncomment if need to debug
+    server.sendmail(FROM_ADDR, TO_ADDRS, message)
+    server.quit()
+
+def send_email_alert_job_not_scheduled(job_name):
+
+    subject = f"[ALERT] {job_name} is neither running nor scheduled to run"
+    body = f"""
+***ALERT: {job_name} is neither RUNNING nor SCHEDULED! Alert someone at Eng WG***
+
+Please reply to this email once the issue has been taken care of, or if you are in the process of doing that, should new alerts be sent again.
+
+If unsure what to do, please post in the #bigscience-engineering slack channel.
+
+*** Useful info ***
+
+On call info: https://github.com/bigscience-workshop/bigscience/tree/master/train/tr1-13B-base#on-call
+Training logs: https://github.com/bigscience-workshop/bigscience/tree/master/train/tr1-13B-base#watching-the-training-logs
+Launching training: https://github.com/bigscience-workshop/bigscience/tree/master/train/tr1-13B-base#training-scripts
+"""
+
+    send_email(subject, body)
+
+def check_running_on_jean_zay():
+    fqdn = socket.getfqdn()
+    if "idris.fr" not in fqdn:
+        raise ValueError("This script relies on JZ's specific environment and won't work elsewhere. "
+        f"You're attempting to run it on '{fqdn}'.")
 
 def run_cmd(cmd):
     try:
@@ -51,6 +97,7 @@ def get_slurm_group_status():
     lines = [line.strip() for line in data.split("\n")]
     return lines
 
+
 def get_remaining_time(time_str):
     """
     slurm style time_str = "2021-08-06T15:23:46"
@@ -61,27 +108,37 @@ def get_remaining_time(time_str):
     delta -= timedelta(microseconds=delta.microseconds)
     return delta
 
+
+def get_preamble():
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # add a string that is easy to grep for:
+    return f"[{timestamp}] PULSE:"
+
+
 def process_job(jobid, partition, name, state, time, nodes, start_time, notes):
 
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     job_on_partition = f"{jobid} on '{partition}' partition"
+    preamble = get_preamble()
 
     if state == "RUNNING":
-        print(f"[{timestamp}] {name} is running for {time} since {start_time} ({job_on_partition} ({notes})")
+        print(f"{preamble} {name} is running for {time} since {start_time} ({job_on_partition} ({notes})")
     elif state == "PENDING":
         if start_time == "N/A":
             if notes == "(JobArrayTaskLimit)":
-                print(f"[{timestamp}] {name} is waiting for the previous Job Array job to finish before scheduling a new one ({job_on_partition})")
+                print(f"{preamble} {name} is waiting for the previous Job Array job to finish before scheduling a new one ({job_on_partition})")
+            elif notes == "(Dependency)":
+                print(f"{preamble} {name} is waiting for the previous job to finish before scheduling a new one using the dependency mechanism ({job_on_partition})")
             else:
-                print(f"[{timestamp}] {name} is waiting to be scheduled ({job_on_partition})")
+                print(f"{preamble} {name} is waiting to be scheduled ({job_on_partition})")
         else:
             remaining_wait_time = get_remaining_time(start_time)
-            print(f"[{timestamp}] {name} is scheduled to start in {remaining_wait_time} (at {start_time}) ({job_on_partition})")
+            print(f"{preamble} {name} is scheduled to start in {remaining_wait_time} (at {start_time}) ({job_on_partition})")
 
         return True
     else:
         # Check that we don't get some 3rd state
-        print(f"[{timestamp}] {name} is unknown - fix me: (at {start_time}) ({job_on_partition}) ({notes})")
+        print(f"{preamble} {name} is unknown - fix me: (at {start_time}) ({job_on_partition}) ({notes})")
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -89,7 +146,10 @@ def get_args():
     parser.add_argument("-d", "--debug", action='store_true', help="enable debug")
     return parser.parse_args()
 
+
 def main():
+
+    check_running_on_jean_zay()
 
     args = get_args()
     status_lines = get_slurm_group_status()
@@ -105,8 +165,9 @@ def main():
             process_job(jobid, partition, name, state, time, nodes, start_time, notes)
 
     if not in_the_system:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"[{timestamp}] ***ALERT: {args.job_name} is not RUNNING or SCHEDULED! Alert someone at Eng WG***")
+        preamble = get_preamble()
+        print(f"{preamble} ***ALERT: {args.job_name} is not RUNNING or SCHEDULED! Alert someone at Eng WG***")
+        send_email_alert_job_not_scheduled(args.job_name)
 
 
 if __name__ == "__main__":
