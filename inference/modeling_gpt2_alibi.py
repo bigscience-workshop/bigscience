a8ade505b7e0a89f8110f8b1b6218b5d1816c45e
ontocord <ontocord@gmail.com>
2021-09-25 10:49:38 -0400

    Update modeling_gpt2_alibi.py

--- inference/modeling_gpt2_alibi.py
+++ inference/modeling_gpt2_alibi.py
@@ -51,7 +51,7 @@ from transformers.modeling_utils import (
 )
 from transformers.utils import logging
 from transformers.utils.model_parallel_utils import assert_device_map, get_device_map
-from transformers.model.gpt2.configuration_gpt2 import GPT2Config
+from transformers.models.gpt2.configuration_gpt2 import GPT2Config
 
 
 logger = logging.get_logger(__name__)
