3005b5fc85f5efcf30123017d848417ebd4829b5
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-12-10 15:14:19 +0100

    Removed HeadQA as it's problematic

---
+++ train/tr11-zero-shot-evaluation/convert_results_to_csv.py
@@ -0,0 +1,57 @@
+import json
+
+def main():
+    path_base = "/gpfsscratch/rech/six/commun/synched_exps/tr11-zero-shot-evaluation/results"
+    output_path = f"{path_base}/final.json"
+    experiments = [
+        "tr3d-1B3-oscar-checkpoints",
+        "tr3e-1B3-c4-checkpoints",
+        "tr3m-1B3-pile-checkpoints"
+    ]
+    # It's very important we keep the ordering.
+    checkpoint_steps = [
+        19500,
+        28500,
+        37500,
+        48000,
+        57000,
+        66000,
+        76500,
+        85500,
+        94500,
+        105000,
+        114000
+    ]
+
+    # We merge all results in a single json
+    merged_json = {}
+    for experiment in experiments:
+        merged_json[experiment] = {}
+        for ckpt_step in checkpoint_steps:
+            absolute_path = f"{path_base}/pretrained=bigscience/{experiment},revision=global_step{ckpt_step}.text"
+            with open(absolute_path, 'r') as fi:
+                results = json.load(fi)["results"]
+
+            for task in results:
+                if task not in merged_json[experiment]:
+                    merged_json[experiment][task] = {}
+
+                for metric in results[task]:
+                    if metric not in merged_json[experiment][task]:
+                        merged_json[experiment][task][metric] = []
+
+                    merged_json[experiment][task][metric].append(results[task][metric])
+
+    # check
+    for experiment in merged_json:
+        for task in merged_json[experiment]:
+            for metric in merged_json[experiment][task]:
+                assert len(merged_json[experiment][task][metric]) == len(checkpoint_steps)
+
+    final_results = {"checkpoint_steps": checkpoint_steps, "results": merged_json}
+    with open(output_path, 'w') as fo:
+        json.dump(final_results, fo)
+    pass
+
+if __name__ == "__main__":
+    main()
