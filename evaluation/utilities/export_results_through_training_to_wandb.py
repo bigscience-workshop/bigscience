710fa8b233af7dc8463251cd4295cda4670cb70d
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-12-17 14:25:13 +0100

    Update the code to send only accuracy averages

--- evaluation/utilities/export_results_through_training_to_wandb.py
+++ evaluation/utilities/export_results_through_training_to_wandb.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     parser.add_argument("--input_file", type=str, required=True)
     parser.add_argument("--all_tasks", action="store_true")
     parser.add_argument("--naive_average", action="store_true")
-    parser.add_argument("--one_per_task_average", action="store_true")
+    parser.add_argument("--acc_average", action="store_true")
     args = parser.parse_args()
     with open(args.input_file) as f:
         data = json.load(f)
@@ -19,21 +19,19 @@ if __name__ == "__main__":
                          reinit=True)
         for i, n_tokens in enumerate(tokens):
             all_values = []
-            one_value_per_task = []
+            acc_average = []
             for task, task_results in results.items():
                 values = None
-                added_this_round = False
                 for metric, values in task_results.items():
                     if args.all_tasks:
                         wandb.log({f"{task}_{metric}": values[i], "tokens": tokens[i]})
                     if "stderr" not in metric and "ppl" not in metric:
-                        added_this_round = True
                         all_values.append(values[i])
-                if added_this_round:
-                    one_value_per_task.append(all_values[-1])
+                        if metric == "acc":
+                            acc_average.append(values[i])
             if args.naive_average:
                 wandb.log({f"naive_average": np.mean(all_values), "tokens": tokens[i]})
-            if args.one_per_task_average:
-                wandb.log({f"one_per_task_average": np.mean(one_value_per_task), "tokens": tokens[i]})
+            if args.acc_average:
+                wandb.log({f"acc_average": np.mean(acc_average), "tokens": tokens[i]})
 
         run.finish()
