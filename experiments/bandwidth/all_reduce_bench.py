161870ad94503300f27c7dcf7e7ee2e8ad10991a
Stas Bekman <stas@stason.org>
2022-04-29 11:02:21 -0700

    extend to include hostnames

--- experiments/bandwidth/all_reduce_bench.py
+++ experiments/bandwidth/all_reduce_bench.py
@@ -1,11 +1,12 @@
 # python -m torch.distributed.launch --nproc_per_node=2 all_reduce_bench.py
 
+import argparse
+import fcntl
+import os
+import socket
+import time
 import torch
 import torch.distributed as dist
-import time
-import argparse
-import os
-import fcntl
 
 # note: this benchmark doesn't care how many gpus per node one has
 
@@ -23,7 +24,7 @@ def printflock(*msgs):
         finally:
             fcntl.flock(fh, fcntl.LOCK_UN)
 
-def timed_allreduce(mat, global_rank):
+def timed_allreduce(mat, id):
     pre = time.perf_counter()
     dist.all_reduce(mat)
     printflock(f"ignore me {int(mat[0][0])}")  # required due to lazy evaluation
@@ -32,19 +33,24 @@ def timed_allreduce(mat, global_rank):
     size = M * N * 4 # 4 is fp32
     n = dist.get_world_size()
     busbw = (size / duration) * (2 * (n - 1) / n) * 8
-    printflock(f"{global_rank}:\n",
+    printflock(f"{id}:\n",
                f"duration: {duration:.4f} sec\n",
                f"algo throughput: {tput:.4f} bps, {tput/1e9:.4f} Gbps\n",
                f"busbw: {busbw / 1e9:.4f}  Gbps"
     )
 
 def run(local_rank):
+    hostname = socket.gethostname()
+    id = f"{hostname}:{local_rank}"
     global_rank = dist.get_rank()
-    printflock(f"{global_rank} data size: {M*N*4/1e9} GB")
+
+    printflock(f"{id} data size: {M*N*4/1e9} GB")
     mat = torch.rand(N, M, dtype=torch.float32).cuda(local_rank)
 
-    for _ in range(TRIALS):
-        timed_allreduce(mat, global_rank)
+    for i in range(TRIALS):
+        if global_rank == 0:
+            print(f"\n\n\n-----------trial-{i}----------------")
+        timed_allreduce(mat, id)
 
 def init_processes(local_rank, fn, backend='nccl'):
     torch.cuda.set_device(local_rank)
