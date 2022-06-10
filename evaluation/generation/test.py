7c6c171ed5bd392500d88b3f1c1aa90cac436b39
younesbelkada <younesbelkada@gmail.com>
2022-06-10 16:17:54 +0200

    rm useless file

--- evaluation/generation/test.py
+++
@@ -1,8 +0,0 @@
-text = ''
-while True:
-        try:
-            dummy = raw_input("""Enter the paragraph :""")
-            text += dummy
-        except KeyboardInterrupt:
-            print(text)
-            text = ''
