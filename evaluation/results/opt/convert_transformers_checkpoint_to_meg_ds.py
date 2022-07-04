cc465ca334f0cd12c09658433dda561748d51a63
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-07-05 01:45:19 +0200

    Hope this is better

--- evaluation/results/opt/convert_transformers_checkpoint_to_meg_ds.py
+++ evaluation/results/opt/convert_transformers_checkpoint_to_meg_ds.py
@@ -112,7 +112,7 @@ def merge_layers(layers, num_heads: int, hidden_size: int):
             return torch.reshape(
                 torch.cat(
                     [
-                        layer.view(num_heads, hidden_size // num_heads, 1)
+                        layer.view(num_heads, 1, hidden_size // num_heads)
                         for layer in layers
                     ],
                     dim=1
@@ -124,7 +124,7 @@ def merge_layers(layers, num_heads: int, hidden_size: int):
             return torch.reshape(
                 torch.cat(
                     [
-                        layer.view(num_heads, hidden_size // num_heads, 1, hidden_size)
+                        layer.view(num_heads, 1, hidden_size // num_heads, hidden_size)
                         for layer in layers
                     ],
                     dim=1
